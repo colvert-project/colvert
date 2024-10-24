06784b324873c736c02fdfccdae7f44ca8afbb88
styx0x6 <styx0x6@users.noreply.github.com>
2024-10-24 21:03:39 +0000

    STATIC_ROOT set

--- colvert/settings.py
+++ colvert/settings.py
@@ -20,6 +20,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

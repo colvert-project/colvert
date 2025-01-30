8eb156d743017c3de37a8d1993d6e276b2d126e5
styx0x6 <styx0x6@users.noreply.github.com>
2025-01-30 16:31:33 +0000

    Add custom config loaded from YAML, update about i18n and minor changes

--- colvert/settings.py
+++ colvert/settings.py
@@ -22,22 +22,31 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from colvert.config_loader import ColvertConfig # Absolute import because of docs building needs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Load settings from Colvert's configuration
+config = ColvertConfig()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@m)cs(idp(tvf_*aia)$i($xm%+(4g2@m6n+q-c!!c_3+r_d2b"
+#SECRET_KEY = "django-insecure-@m)cs(idp(tvf_*aia)$i($xm%+(4g2@m6n+q-c!!c_3+r_d2b"
+SECRET_KEY = config.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.debug
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config.allowed_hosts
 
+CSRF_TRUSTED_ORIGINS = config.csrf_trusted_origins
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -49,7 +58,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "whitenoise.runserver_nostatic", # Force `--nostatic` option when running `python manage.py runserver` in development (DEBUG = True)
     "django.contrib.staticfiles",
-    "core",
+    "core", # Colvert's core app
 ]
 
 MIDDLEWARE = [
@@ -61,6 +70,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware", # To manage l10n
 ]
 
 ROOT_URLCONF = "colvert.urls"
@@ -76,6 +86,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "core.context_processors.instance_context", # Core's instance context
+                "core.context_processors.global_context", # Core's global context
             ],
         },
     },
@@ -87,12 +99,7 @@ WSGI_APPLICATION = "colvert.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = config.database
 
 
 # Password validation
@@ -117,13 +124,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Luxembourg"
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
+
+# Add languages you want to support
+LANGUAGES = [
+    ('en', 'English'),
+    ('fr', 'French'),
+    ('de', 'German'),
+]
+
+# Path to the locale directory
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
@@ -139,6 +159,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Colvert - Application constants
+
+DEFAULT_CHARSET = 'utf-8'
 
 APP_TECH_NAME = "colvert"
 APP_SHORT_NAME = "Colvert"
@@ -162,14 +184,13 @@ APP_CONTRIBUTING_MD = "CONTRIBUTING.md"
 APP_COC_MD = "CODE_OF_CONDUCT.md"
 APP_EMPOWERING_TECH = "Django & AdminLTE"
 APP_VERSION = "0.1.0"
-APP_YEARS = "2024"
+APP_YEARS = "2024-2025"
 APP_VERSION_STRING = "%s/%s" % (APP_TECH_NAME, APP_VERSION)
 APP_COPYRIGHT_STRING = "Copyright (C) %s  %s. %s." % (APP_YEARS, APP_CONTRIBUTORS, APP_LICENCE)
-APP_COPYRIGHT_HTML = "Copyright &copy; %s %s. Licensed under the %s." % (APP_YEARS, APP_CONTRIBUTORS, APP_LICENCE)
 APP_CREATED_BY = "Created by %s" % (APP_AUTHORS)
 APP_SPONSORED_BY = "Sponsored by %s" % (APP_SPONSORS)
 APP_POWERED_BY = "Powered by %s" % (APP_EMPOWERING_TECH)
 
 # Colvert - Static String Constants
 
-STR_PFX_DEBUG = "[DEBUG]"
+TITLE_PREFIX = f"[DEBUG] {APP_SHORT_NAME}" if DEBUG else APP_SHORT_NAME
